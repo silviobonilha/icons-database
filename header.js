1661ef3e6f1cba8aa86484a5cdd8d5f66bc46d65
Silvio Bonilha <bonilha.silvio@gmail.com>
2025-12-15 18:51:52 -0300

    Update header.js

--- header.js
+++ header.js
@@ -6,8 +6,8 @@ class CustomHeader extends HTMLElement {
                 <div class="text-center px-4 py-3">
                  <img src="images/logo-educ.png" alt="logotipo" class="w-1/3 h-1/4 object-cover">
                     <!-- a href="index.html" class="text-2xl font-bold hover:text-red-200 transition-colors block" -->
-                        Relatório em Sala de Aula
-                    </a>
+                  <h2>      Relatório em Sala de Aula </h2>
+                    <!--/a -->
                 </div>
 
                 <div class="px-4 pb-3 flex justify-center">

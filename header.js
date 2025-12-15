752ebc130fb96f4c81e8feb5c2acf2d0da5e2cbe
Silvio Bonilha <bonilha.silvio@gmail.com>
2025-12-15 19:55:26 -0300

    Update header.js

--- header.js
+++ header.js
@@ -4,18 +4,14 @@ class CustomHeader extends HTMLElement {
             <header class="bg-red-700 text-white shadow-md max-w-[800px] mx-auto rounded-md">
                 
                 <div class="text-center px-4 py-3">
-<--! substituido  -->
-                 <!--img src="https://raw.githubusercontent.com/silviobonilha/database/refs/heads/image/logo-educ.png" alt="logotipo" class="w-1/3 h-1/3 object-cover" -->
-                        <!--span>Portal de Relatórios </span -->
 
 <img
   src="https://raw.githubusercontent.com/silviobonilha/database/refs/heads/image/logo-educ.png"
   alt="logotipo"
-  class="w-24 h-auto" >
+  class="w-24 h-auto">
                        
-                    <!-- a href="index.html" class="text-2xl font-bold hover:text-red-200 transition-colors block" -->
                   <h1> Relatório em Sala de Aula </h1>
-                    <!--/a -->
+    
                     
                 </div>
 
